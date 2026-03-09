054dd05f913e1161ef0844242361f6b280caee44
sanja_r_q <151444712+sanjar1001@users.noreply.github.com>
2026-03-09 15:50:34 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,47 +1,65 @@
-document.addEventListener("DOMContentLoaded", () => {
+const URL_APP = "https://script.google.com/macros/s/AKfycbx5AEia7SiOzfyhl4inYCnfIyj25bE2mlktgurUHgQFCp-IRBcfLf7QFbULXxU_2daH/exec";
 
-const URL_APP = "https://script.google.com/macros/s/AKfycbwYZ4XGss0c2dgvyEwvjv5H4EJDk00D3cmDd03Fy60LmV3zCef-KhIFKfVhXCbLKV-a/exec";
-
+// находим форму
 const form = document.querySelector("#form");
 
-form.addEventListener("submit", async (e) => {
+// проверка обязательных полей
+function isFilled(details) {
+  const { departament, fio, rank, phone, birthday, car, family, children, rule } = details;
+  if (!departament || !fio || !rank || !phone || !birthday || !car || !family || !children || !rule) return false;
+  return true;
+}
 
-  e.preventDefault();
+// обработчик отправки формы
+form.addEventListener("submit", async (ev) => {
+  ev.preventDefault();
 
-  const formData = new FormData(form);
+  // 2. Теперь собираем данные формы 
+  const details = {
+    departament: form.departament.value,
+    fio: form.fio.value.trim(),
+    rank: form.rank.value,
+    phone: form.phone.value.trim(),
+    birthday: form.birthday.value,
+    car: form.car.value,
+    family: form.family.value,
+    children: form.children.value,
+    rule: form.rule.checked
+  };
 
-  const data = new URLSearchParams();
-
-  for (const pair of formData) {
-    data.append(pair[0], pair[1]);
+  if (!isFilled(details)) return alert("Заполните все обязательные поля");
+  // формируем x-www-form-urlencoded
+  let formBody = [];
+  for (let key in details) {
+    formBody.push(encodeURIComponent(key) + "=" + encodeURIComponent(details[key]));
   }
+  formBody = formBody.join("&");
 
   try {
-
-    const response = await fetch(URL_APP, {
+    const result = await fetch(URL_APP, {
       method: "POST",
-      body: data
-    });
-
-    const result = await response.json();
+      headers: { "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8" },
+      body: formBody
+    }).then(res => res.json());
 
     if (result.type === "success") {
-
       form.reset();
       alert("Спасибо! Заявка отправлена");
-
     } else {
-
       alert("Ошибка: " + JSON.stringify(result.errors));
-
     }
-
-  } catch (error) {
-
+  } catch (err) {
     alert("Ошибка отправки");
-
   }
-
 });
 
+window.addEventListener("load", function () {
+  setTimeout(function () {
+    document.getElementById("splash").style.opacity = "0";
+    setTimeout(function () {
+      document.getElementById("splash").style.display = "none";
+      document.getElementById("main-content").style.display = "block";
+    }, 600);
+  }, 2000);
 });
+
